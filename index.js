57c2348d0b2e8af531044cc6faa55564c623c49e
Rathodnitisha <nitisharathod07@gmail.com>
2025-07-14 11:41:01 +0530

    project

--- index.js
+++ index.js
@@ -1,43 +1,52 @@
 
-console.log("Hello")
+// console.log("Hello")
 
-const name = "Nitisha"
-console.log(name)
+// const name = "Nitisha"
+// console.log(name)
 
-const a=7
-if (a%2==0)
+// const a=7
+// if (a%2==0)
+// {
+//     console.log("number is  even")
+// }
+// else{
+//     console.log("number is odd")
+// }
+
+// console.log("Atmiyaaaa")
+
+// var num1=10
+// var num2=20
+// if(num1>=num2){
+//     console.log("num1 is big")
+// }
+// else{
+//     console.log("num2 is big")
+// }
+
+
+// const year=2024
+// if(year%4==0){
+//     console.log("leap year")
+// }
+// else{
+//     console.log("not leap year")
+// }
+
+
+// var num=5
+// var sum=0
+// for(let i=1;i<=num;i++)
+// {
+//     sum+=i
+// }
+// console.log(sum)
+
+
+var num=4
+var f=2
+for(var i=num;i>0;i--)
 {
-    console.log("number is  even")
+    f*=i
 }
-else{
-    console.log("number is odd")
-}
-
-console.log("Atmiyaaaa")
-
-var num1=10
-var num2=20
-if(num1>=num2){
-    console.log("num1 is big")
-}
-else{
-    console.log("num2 is big")
-}
-
-
-const year=2024
-if(year%4==0){
-    console.log("leap year")
-}
-else{
-    console.log("not leap year")
-}
-
-
-var num=5
-var sum=0
-for(let i=1;i<=num;i++)
-{
-    sum+=i
-}
-console.log(sum)
+console.log(f)
