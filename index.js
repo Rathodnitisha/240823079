2738a6cb32bd39939fb94b82aeb5140f72dd438e
atmiya <nitisharathod07@gmail.com>
2025-07-22 13:21:37 +0530

    project

--- index.js
+++ index.js
@@ -136,56 +136,121 @@
 
 
 // pro-12
-let sum = 0;
-const number = 153
-let temp = number;
-while (temp > 0) {
+// let sum = 0;
+// const number = 153
+// let temp = number;
+// while (temp > 0) {
    
-    let remainder = temp % 10;
-    sum += remainder * remainder * remainder;
-    temp = parseInt(temp / 10);
-}
-if (sum == number) {
-    console.log(`${number} is an Armstrong number`);
-}
-else {
-    console.log(`${number} is not an Armstrong number.`);
-}
+//     let remainder = temp % 10;
+//     sum += remainder * remainder * remainder;
+//     temp = parseInt(temp / 10);
+// }
+// if (sum == number) {
+//     console.log(`${number} is an Armstrong number`);
+// }
+// else {
+//     console.log(`${number} is not an Armstrong number.`);
+// }
 
 
 // pro-13
 
-const number = 5;
-let n1 = 0, n2 = 1, nextTerm;
-for (let i = 1; i <= number; i++) {
-    console.log(n1);
-    nextTerm = n1 + n2;
-    n1 = n2;
-    n2 = nextTerm;
-}
+// const number = 5;
+// let n1 = 0, n2 = 1, nextTerm;
+// for (let i = 1; i <= number; i++) {
+//     console.log(n1);
+//     nextTerm = n1 + n2;
+//     n1 = n2;
+//     n2 = nextTerm;
+// }
 
 // pro-14
 
-let s = 'g';
-if (s === 'a' || s === 'e' || s === 'i' || s === 'o' || s === 'u' ||
-    s === 'A' || s === 'E' || s === 'I' || s === 'O' || s === 'U') {
-    console.log(`${s} is a vowel`);
-} else {
-    console.log(`${s} is a consonant`);
-}
+// let s = 'g';
+// if (s === 'a' || s === 'e' || s === 'i' || s === 'o' || s === 'u' ||
+//     s === 'A' || s === 'E' || s === 'I' || s === 'O' || s === 'U') {
+//     console.log(`${s} is a vowel`);
+// } else {
+//     console.log(`${s} is a consonant`);
+// }
 
 // program-15
-let x = 4
-let y = 2;
-let a = x + y;
-let b = x - y;
-let c = x*y;
-let d = x/y;
-console.log(a);
-console.log(b);
-console.log(c);
-console.log(d);
+// let x = 4
+// let y = 2;
+// let a = x + y;
+// let b = x - y;
+// let c = x*y;
+// let d = x/y;
+// console.log(a);
+// console.log(b);
+// console.log(c);
+// console.log(d);
+
+
+// pro-16
+// Input
+// let num1 = 20;
+// let num2 = 28;
+
+// while (num2 !== 0) {
+//     let temp = num2;
+//     num2 = num1 % num2;
+//     num1 = temp;
+// }
+
+// console.log(`GCD is: ${num1}`);
+
 
  
-  
-  
+// pro-17
+
+// let number = 28;
+// let sum = 0;
+
+// for (let i = 1; i <= number / 2; i++) {
+//     if (number % i === 0) {
+//         sum += i;
+//     }
+// }
+
+// if (sum === number) {
+//     console.log(`${number} is a Perfect Number`);
+// } else {
+//     console.log(`${number} is Not a Perfect Number`);
+// }
+
+
+// pro-18
+// let number = 10;
+
+// console.log(`Divisors of ${number} are:`);
+
+// for (let i = 1; i <= number; i++) {
+//     if (number % i === 0) {
+//         process.stdout.write(i + " ");
+//     }
+// }
+// console.log(); 
+
+
+// pro-19
+// let number = -5;
+
+// if (number > 0) {
+//     console.log("Positive");
+// } else if (number < 0) {
+//     console.log("Negative");
+// } else {
+//     console.log("Zero");
+// }
+
+
+// pro-20
+let a = 2;
+let b = 3;
+let result = 1;
+for (let i = 1; i <= b; i++) {
+    result *= a;
+}
+console.log(result);
+
