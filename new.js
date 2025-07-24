0a1128d3232d9c65203d84c2c2831d277ca0edee
Rathodnitisha <nitisharathod07@gmail.com>
2025-07-24 08:54:19 +0530

    project

---
+++ new.js
@@ -0,0 +1,26 @@
+// pro-1(file-path)
+const path = require("path");
+console.log(__filename);
+console.log(path.dirname(__filename));
+console.log(path.extname("myfile.txt"));
+
+// pro-2(file-moment)
+const moment = require("moment");
+console.log(moment().format("DD-MM-YYYY HH:MM"));
+console.log(moment().add(7,"days").format("DD-MM-YYYY HH:MM"));
+console.log(moment().format('DDDD'));
+console.log(moment());
+
+// pro-3(file-fs.write)
+const fs = require("fs");
+fs.writeFile("txt.txt","hellow",function(err){
+      if (err)throw err;
+      console.log("file save!");
+});
+
+// (file-fs.read)
+fs.readFile("txt.txt","utf8",function(err,data){
+      if (err)throw err;
+      console.log("file content:",data);
+});
+
