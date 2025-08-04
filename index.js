389abca38ee07d2db106100376a23f9148a258c9
Rathodnitisha <nitisharathod07@gmail.com>
2025-08-04 11:34:51 +0530

    project

--- index.js
+++ index.js
@@ -6,16 +6,16 @@ app.get("/",(req,res)=>{
     res.send("hello,Mca student!\n");
 });
 app.get("/show/:id",(req,res)=>{
-    res.send(`hello,student!\n ID: ${req.params.id}`);
+    res.send(`hello,student!\n ID:${req.params.id}`);
 });
 app.post("/store",(req,res)=>{
-    res.send("successfully stored record\n ${req.body.name}, $(req.body.city}");
+    res.send(`successfully stored record\n ${req.body.name}, ${req.body.city}`);
 });
 app.put("/update/:id",(req,res)=>{
-    res.send("successfully updated record\nID: ${req.params.id}" );
+    res.send(`successfully updated record\nID: ${req.params.id}, name:${req.body.name}, city:${req.body.city}`);
 });
 app.delete("/delete/:id",(req,res)=>{
-    res.send("successfully deleted record with ID: ${req.params.id}");
+    res.send(`successfully deleted record with ID: ${req.params.id}`);
 });
 app.listen(PORT,()=>{
     console.log(`server is running at http://localhost:${PORT}/`);
